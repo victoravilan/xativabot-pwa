eb4d0fc33f0b11c546b20e0e6570e65f46ca3c53
victoravilan <victoravilan@gmail.com>
2025-09-08 16:06:38 +0200

    Add files via upload

--- netlify/functions/season.js
+++ netlify/functions/season.js
@@ -15,6 +15,55 @@ const SEED_ES = {
  12: { fruit: ["kiwi","manzana","pera","mandarina","naranja","pomelo"], veg: ["acelga","col","brócoli","coliflor","espinaca","puerro"] }
 };
 
+{
+  "meta": { "country": "ES", "months": [1,2,3,4,5,6,7,8,9,10,11,12] },
+  "produce": [
+    { "id": "alcachofa", "kind": "veg", "name": {"es":"alcachofa","en":"artichoke","ca":"carxofa"}, "months": [1,2,3,11,12] },
+    { "id": "berenjena", "kind": "veg", "name": {"es":"berenjena","en":"eggplant","ca":"albergínia"}, "months": [6,7,8,9,10] },
+    { "id": "calabacin", "kind": "veg", "name": {"es":"calabacín","en":"courgette","ca":"carbassó"}, "months": [5,6,7,8,9,10] },
+    { "id": "calabaza", "kind": "veg", "name": {"es":"calabaza","en":"pumpkin","ca":"carabassa"}, "months": [9,10,11,12,1,2] },
+    { "id": "cebolla", "kind": "veg", "name": {"es":"cebolla","en":"onion","ca":"ceba"}, "months": [5,6,7,8,9,10,11,12] },
+    { "id": "cebolleta", "kind": "veg", "name": {"es":"cebolleta","en":"spring onion","ca":"cebolleta"}, "months": [3,4,5] },
+    { "id": "coliflor", "kind": "veg", "name": {"es":"coliflor","en":"cauliflower","ca":"coliflor"}, "months": [11,12,1,2,3,4] },
+    { "id": "brocoli", "kind": "veg", "name": {"es":"brócoli","en":"broccoli","ca":"bròquil"}, "months": [10,11,12,1,2,3,4] },
+    { "id": "esparrago", "kind": "veg", "name": {"es":"espárrago","en":"asparagus","ca":"espàrrec"}, "months": [3,4,5] },
+    { "id": "espinaca", "kind": "veg", "name": {"es":"espinaca","en":"spinach","ca":"espinac"}, "months": [11,12,1,2,3,4] },
+    { "id": "acelga", "kind": "veg", "name": {"es":"acelga","en":"chard","ca":"bledes"}, "months": [10,11,12,1,2,3,4,5] },
+    { "id": "lechuga", "kind": "veg", "name": {"es":"lechuga","en":"lettuce","ca":"enciam"}, "months": [3,4,5,6,10,11] },
+    { "id": "pimiento", "kind": "veg", "name": {"es":"pimiento","en":"pepper","ca":"pebrot"}, "months": [6,7,8,9,10] },
+    { "id": "tomate", "kind": "veg", "name": {"es":"tomate","en":"tomato","ca":"tomàquet"}, "months": [6,7,8,9,10] },
+    { "id": "pepino", "kind": "veg", "name": {"es":"pepino","en":"cucumber","ca":"cogombre"}, "months": [5,6,7,8,9] },
+    { "id": "zanahoria", "kind": "veg", "name": {"es":"zanahoria","en":"carrot","ca":"pastanaga"}, "months": [10,11,12,1,2,3,4,5] },
+    { "id": "puerro", "kind": "veg", "name": {"es":"puerro","en":"leek","ca":"porro"}, "months": [10,11,12,1,2,3,4] },
+    { "id": "remolacha", "kind": "veg", "name": {"es":"remolacha","en":"beetroot","ca":"remolatxa"}, "months": [5,6,7,8,9,10] },
+    { "id": "patata", "kind": "veg", "name": {"es":"patata","en":"potato","ca":"creïlla"}, "months": [5,6,7,8,9,10,11] },
+    { "id": "fresa", "kind": "fruit", "name": {"es":"fresa","en":"strawberry","ca":"maduixa"}, "months": [3,4,5,6] },
+    { "id": "cereza", "kind": "fruit", "name": {"es":"cereza","en":"cherry","ca":"cirera"}, "months": [5,6] },
+    { "id": "albaricoque", "kind": "fruit", "name": {"es":"albaricoque","en":"apricot","ca":"albercoc"}, "months": [5,6,7] },
+    { "id": "melocoton", "kind": "fruit", "name": {"es":"melocotón","en":"peach","ca":"préssec"}, "months": [6,7,8,9] },
+    { "id": "nectarina", "kind": "fruit", "name": {"es":"nectarina","en":"nectarine","ca":"nectarina"}, "months": [6,7,8,9] },
+    { "id": "ciruela", "kind": "fruit", "name": {"es":"ciruela","en":"plum","ca":"pruna"}, "months": [6,7,8,9] },
+    { "id": "sandia", "kind": "fruit", "name": {"es":"sandía","en":"watermelon","ca":"síndria"}, "months": [6,7,8,9] },
+    { "id": "melon", "kind": "fruit", "name": {"es":"melón","en":"melon","ca":"meló"}, "months": [6,7,8,9] },
+    { "id": "uva", "kind": "fruit", "name": {"es":"uva","en":"grape","ca":"raïm"}, "months": [8,9,10] },
+    { "id": "manzana", "kind": "fruit", "name": {"es":"manzana","en":"apple","ca":"poma"}, "months": [9,10,11,12,1,2] },
+    { "id": "pera", "kind": "fruit", "name": {"es":"pera","en":"pear","ca":"pera"}, "months": [8,9,10,11] },
+    { "id": "caqui", "kind": "fruit", "name": {"es":"caqui","en":"persimmon","ca":"cacauet/caqui"}, "months": [10,11,12] },
+    { "id": "granada", "kind": "fruit", "name": {"es":"granada","en":"pomegranate","ca":"magrana"}, "months": [9,10,11,12] },
+    { "id": "naranja", "kind": "fruit", "name": {"es":"naranja","en":"orange","ca":"taronja"}, "months": [11,12,1,2,3,4] },
+    { "id": "mandarina", "kind": "fruit", "name": {"es":"mandarina","en":"tangerine","ca":"mandarina"}, "months": [10,11,12,1,2] },
+    { "id": "limon", "kind": "fruit", "name": {"es":"limón","en":"lemon","ca":"llimona"}, "months": [11,12,1,2,3,4,5] },
+    { "id": "pomelo", "kind": "fruit", "name": {"es":"pomelo","en":"grapefruit","ca":"aranja/pomelo"}, "months": [11,12,1,2,3] },
+    { "id": "higo", "kind": "fruit", "name": {"es":"higo","en":"fig","ca":"figa"}, "months": [8,9] },
+    { "id": "chirimoya", "kind": "fruit", "name": {"es":"chirimoya","en":"cherimoya","ca":"xirimoia"}, "months": [10,11,12,1,2] },
+    { "id": "kiwi", "kind": "fruit", "name": {"es":"kiwi","en":"kiwifruit","ca":"kiwi"}, "months": [11,12,1,2,3,4] },
+    { "id": "platano", "kind": "fruit", "name": {"es":"plátano (Canarias)","en":"banana/plantain","ca":"plàtan"}, "months": [1,2,3,4,5,6,7,8,9,10,11,12] },
+    { "id": "aguacate", "kind": "fruit", "name": {"es":"aguacate","en":"avocado","ca":"alvocat"}, "months": [11,12,1,2,3,4] },
+    { "id": "mango", "kind": "fruit", "name": {"es":"mango (tropical Málaga)","en":"mango","ca":"mango"}, "months": [9,10,11] }
+  ]
+}
+
+
 export default async (req) => {
   const { searchParams } = new URL(req.url);
   const country = (searchParams.get('country') || 'ES').toUpperCase();
